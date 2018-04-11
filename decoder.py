43a05a1a30a478efaf6cc5035bf8a9ab616b12a3
Mihro <officialmihro@gmail.com>
2018-04-11 03:06:48 +0100

    Started basic 1-dim pass on voxels
    Finds all unique y,z coords containing voxels. Iterates through x coords
    and adds coord to temp list if not consecutive to previous.

--- decoder.py
+++ decoder.py
@@ -1,6 +1,7 @@
 import struct
 file = "test.vox"
 
+
 def importVoxels(path):
     voxel_list = []  
     with open(path, 'rb') as f:
@@ -19,12 +20,27 @@ def importVoxels(path):
             print("Reading", numvoxels, "voxels...")
             for x in range(0, numvoxels):
                 bytes = f.read(4)
-                voxel = struct.unpack('bbbB', bytes)
+                voxel = struct.unpack('bbbb', bytes)
                 voxel_list.append(voxel)
         voxel_list.sort(key=lambda v:(v[2],v[1],v[0]))
             
     return dimensions, voxel_list
 
+def pass_1D(dimensions,voxels):
+    objects = []
+    object_count = 0
+    plane_coords = sorted(set([v[1:3] for v in voxels]))
+    for y,z in plane_coords:
+        prev_x = None
+        x_voxels = list(filter(lambda v: v[1]==y and v[2]==z, voxels))
+        for x in enumerate([coord[0] for coord in x_voxels]):
+            if x[1]-1 != prev_x:
+                objects.append(x_voxels[x[0]])
+            prev_x = x[1]
+    print(objects,"\n")
+
 dimensions, voxels = importVoxels(file)
+pass_1D(dimensions, [v[:3] for v in voxels])
+
 
 print("Completed!")
